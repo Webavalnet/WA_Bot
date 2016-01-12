ca706fdd76dba37a4bc36380faefb861606e03d4
mobinantispam <m.fdostiha@gmail.com>
2016-01-12 08:38:16 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,7 +221,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {85831686,134177004},--Sudo users
+    sudo_users = {155933792,85831686,134177004},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
